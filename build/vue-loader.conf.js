f2db23333a9ec55f1b1c7d2001ddc5d57b2f5fe4
yanxiaodi <929213769@qq.com>
2017-02-22 23:43:24 +0800

    :art: 优化'服务项目'内容样式

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -3,16 +3,12 @@ var config = require('../config')
 var isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // loaders: utils.cssLoaders({
-    //     sourceMap: isProduction
-    //     ? config.build.productionSourceMap
-    //     : config.dev.cssSourceMap,
-    //     extract: isProduction
-    // }),
-    loaders: {
-        sass: 'style!css!sass?indentedSyntax',
-        scss: 'style!css!sass'
-    }
+    loaders: utils.cssLoaders({
+        sourceMap: isProduction
+        ? config.build.productionSourceMap
+        : config.dev.cssSourceMap,
+        extract: isProduction
+    })
     ,postcss: [
         require('autoprefixer')({
         browsers: ['last 10 versions']
--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,3 +1,67 @@
-<template>
-
+<template lang="pug">
+#Service
+    mu-row( gutter )
+        mu-col( v-for=" item in serviceArr " class="serviceBox" width="100" tablet="33" desktop="33" )
+            div
+                .imgBox
+                    img( v-bind:src=" item.img " )
+                h2 {{ item.title }}
+                p( v-bind:style=" { display: 'block' } " ) {{ item.text_A }}
+                p( v-bind:style=" { display: 'block' } " ) {{ item.text_B }}
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            serviceArr: [
+                {
+                    img     : require('../assets/img/ico-tool-30-px@3x.png')
+                    ,title  : '行业解决方案'
+                    ,text_A : '技术驱动商业创新'
+                    ,text_B : '研发符合企业需求的产品'
+                }
+                ,{
+                    img     : require('../assets/img/ico-software-30-px@3x.png')
+                    ,title  : '软件解决方案'
+                    ,text_A : '多行业应用产品'
+                    ,text_B : '满足不同行业的需求'
+                }
+                ,{
+                    img     : require('../assets/img/ico-manager-30-px@3x.png')
+                    ,title  : '个性化定制'
+                    ,text_A : '产品经理跟踪制'
+                    ,text_B : '为客户量身订制'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="sass?indentedSyntax" scoped>
+@import '../sass/main'
+
+#Service
+    +moduleBoxStyle
+    .serviceBox
+        +flexCenter
+        +textCenter
+        +REM( padding, $M-padding )
+        +REM( margin-bottom, $M-padding )
+        +bC( $F )
+        >div
+            .imgBox
+                @extend %dib
+                +REM-W-H( 30px, 30px )
+                >img
+                    +imgCover( 100% )
+            h2
+                +REM( font-size, $F-title )
+                +REM( margin-top, $M-contentMargin )
+                +REM( margin-bottom, $M-contentMargin )
+            p
+                +REM( font-size, $F-text )
+                +REM( line-height, $F-title )
+                color: $C-text
+</style>
