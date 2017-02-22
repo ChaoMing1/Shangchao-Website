7e83d0ba7bd4f79d5a00495af784196f985661e4
yanxiaodi <929213769@qq.com>
2017-02-22 18:28:55 +0800

    :art: 完成'关于我们'移动端 + iPad端样式

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,24 @@
 #app
     AppBar
     Banner
+    .moduleBox( v-bind:style=" { backgroundColor: '#FFF' } " )
+        <TitleBox :title_info = aboutUs />
+        AboutUs
+    .moduleBox
+        <TitleBox :title_info = service />
+        Service
+    .moduleBox( v-bind:style=" { backgroundColor: '#FFF' } " )
+        <TitleBox :title_info = works />
+        Works
+    .moduleBox
+        <TitleBox :title_info = team />
+        Team
+    .moduleBox( v-bind:style=" { backgroundColor: '#000' } " )
+        <TitleBox :title_info = joinUs />
+        JoinUs
+    .moduleBox
+        <TitleBox :title_info = contactUs />
+        ContactUs
     MapView
     FooterBox
 </template>
@@ -9,12 +27,151 @@
 <script>
 import AppBar       from './components/AppBar'
 import Banner       from './components/Banner'
+import TitleBox     from './components/TitleBox'
+
+import AboutUs      from './components/AboutUs'
+import Service      from './components/Service'
+import Works        from './components/Works'
+import Team         from './components/Team'
+import JoinUs       from './components/JoinUs'
+import ContactUs    from './components/ContactUs'
+
 import MapView      from './components/MapView'
 import FooterBox    from './components/FooterBox'
-const components = { AppBar, Banner, MapView, FooterBox }
+const components = { AppBar, Banner, TitleBox, AboutUs, Service, Works, Team, JoinUs, ContactUs, MapView, FooterBox }
 
 export default {
     name: 'app'
+    ,data() {
+        return {
+            // 关于我们
+            aboutUs: {
+                title: [
+                    {
+                        text   : '关于'
+                        ,color : 'rgb( 51,51,51 )'
+                    }
+                    ,{
+                        text   : '我们'
+                        ,color : 'rgb( 216, 59, 16 )'
+                    }
+                ]
+                ,subTitle: {
+                    text    : 'WHO WE ARE'
+                    ,color  : 'rgb( 85,85,85 )'
+                }
+                ,icon: {
+                    class: 'person'
+                    ,color: 'rgb( 110,110,110 )'
+                }
+            }
+            // 服务项目
+            ,service: {
+                title: [
+                    {
+                        text   : '服务'
+                        ,color : 'rgb( 51,51,51 )'
+                    }
+                    ,{
+                        text   : '项目'
+                        ,color : 'rgb( 216, 59, 16 )'
+                    }
+                ]
+                ,subTitle: {
+                    text    : 'WHAT WE DOING'
+                    ,color  : 'rgb( 85,85,85 )'
+                }
+                ,icon: {
+                    class: 'settings'
+                    ,color: 'rgb( 110,110,110 )'
+                }
+            }
+            // 成功案例
+            ,works: {
+                title: [
+                    {
+                        text   : '成功'
+                        ,color : 'rgb( 51,51,51 )'
+                    }
+                    ,{
+                        text   : '案例'
+                        ,color : 'rgb( 216, 59, 16 )'
+                    }
+                ]
+                ,subTitle: {
+                    text    : 'OUR LATEST WORKS'
+                    ,color  : 'rgb( 85,85,85 )'
+                }
+                ,icon: {
+                    class: 'photo'
+                    ,color: 'rgb( 110,110,110 )'
+                }
+            }
+            // 团队介绍
+            ,team: {
+                title: [
+                    {
+                        text   : '团队'
+                        ,color : 'rgb( 51,51,51 )'
+                    }
+                    ,{
+                        text   : '介绍'
+                        ,color : 'rgb( 216, 59, 16 )'
+                    }
+                ]
+                ,subTitle: {
+                    text    : 'OUR TEAM'
+                    ,color  : 'rgb( 85,85,85 )'
+                }
+                ,icon: {
+                    class: 'settings'
+                    ,color: 'rgb( 110,110,110 )'
+                }
+            }
+            // 招贤纳士
+            ,joinUs: {
+                title: [
+                    {
+                        text   : '招贤'
+                        ,color : 'rgb( 250, 250, 250 )'
+                    }
+                    ,{
+                        text   : '纳士'
+                        ,color : 'rgb( 250, 250, 250 )'
+                    }
+                ]
+                ,subTitle: {
+                    text    : 'JOIN US'
+                    ,color  : 'rgb( 250, 250, 250 )'
+                }
+                ,icon: {
+                    class: 'group'
+                    ,color: 'rgb( 110,110,110 )'
+                }
+            }
+            // 联系我们
+            ,contactUs: {
+                title: [
+                    {
+                        text   : '联系'
+                        ,color : 'rgb( 51,51,51 )'
+                    }
+                    ,{
+                        text   : '我们'
+                        ,color : 'rgb( 216, 59, 16 )'
+                    }
+                ]
+                ,subTitle: {
+                    text    : 'GET IN TOUCH'
+                    ,color  : 'rgb( 85,85,85 )'
+                }
+                ,icon: {
+                    class: 'call'
+                    ,color: 'rgb( 110,110,110 )'
+                }
+            }
+        }
+    }
     ,components: components
 }
 </script>
---
+++ src/components/TitleBox.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+.title
+    div
+        h2( v-bind:style = " { color: title_info.title[0].color,display: 'inline-block' } " ) {{ title_info.title[0].text }}
+        h2( v-bind:style = " { color: title_info.title[1].color,display: 'inline-block' } " ) {{ title_info.title[1].text }}
+        p( v-bind:style = " { color: title_info.subTitle.color } " ) {{ title_info.subTitle.text }}
+        .modifiedLine
+            .string( v-bind:style = " { left: '0', backgroundColor: title_info.icon.color } " )
+            i( class="mu-icon material-icons" v-bind:style = " { color: title_info.icon.color } " ) {{ title_info.icon.class }}
+            .string( v-bind:style = " { right: '0', backgroundColor: title_info.icon.color } " )
+</template>
+
+<script>
+export default {
+    props: ['title_info']
+}
+</script>
+
+<style lang="sass?indentedSyntax" scoped>
+@import '../sass/main'
+
+.title
+    +flexCenter
+    +textCenter
+    +REM( padding-top, $M-padding )
+    +REM( padding-bottom, $M-padding )
+    >div
+        +W100
+        h2
+            +REM( font-size, $F-title )
+        p
+            +REM( font-size, $F-text )
+            +REM( margin-top, $M-contentMargin )
+            +REM( margin-bottom, $M-contentMargin )
+        .modifiedLine
+            +REL
+            width: 100%
+            .string
+                display: block
+                +ABS
+                top: 46%
+                z-index: 1
+                width: 40%
+                +REM( height, 1px )
+                border-top: 1px solid $C-text
+                +REM( border-bottom-width, 1px )
+</style>
